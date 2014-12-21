f39ebee93ac7578b257bd3d734dc8c7e33931f39
AShevtsov <shevcov1994@mail.ru>
2014-12-22 00:07:37 +0300

    Logout has been fixed; Fixed problem incorrect redirect after logout;

--- ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/logout/LogoutManagedBean.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/logout/LogoutManagedBean.java
@@ -1,19 +1,26 @@
 package com.comicszone.managedbeans.logout;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
 @ManagedBean
 @RequestScoped
 public class LogoutManagedBean {
-    public String logout(){
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        if (facesContext == null) return "/resources/templates/index.jsf";
-        HttpSession httpSession = (HttpSession)facesContext.getExternalContext().getSession(false);
-        if (httpSession == null) return "/resources/templates/index.jsf" ;
-        httpSession.invalidate();
-        return "/resources/templates/index.jsf";
+    public void logout(){
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            ExternalContext ex = facesContext.getExternalContext();
+            HttpSession httpSession = (HttpSession)facesContext.getExternalContext().getSession(false);
+            httpSession.invalidate();
+            ex.redirect("/ComicsZoneTracker/resources/templates/index.jsf");
+        } catch (IOException ex1) {
+            Logger.getLogger(LogoutManagedBean.class.getName()).log(Level.SEVERE, null, ex1);
+        }
     }
 }
