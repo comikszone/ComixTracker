db929238f2cdf8311de19297ab9e4c0478600649
MrFigureSkating <aistlentyev@gmail.com>
2014-12-28 00:20:11 +0300

    app can get json of personal information

--- ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/social_networks/SocialNetwork.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/social_networks/SocialNetwork.java
@@ -5,12 +5,18 @@
  */
 package com.comicszone.managedbeans.social_networks;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson.JacksonFactory;
+import java.io.IOException;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -25,7 +31,7 @@ import javax.faces.bean.SessionScoped;
 public class SocialNetwork {
     private static final String CLIENT_ID = "232041634310-t8k3nf1cbede85kbu8ljsc16j2fdfbvf.apps.googleusercontent.com";
     private static final String CLIENT_SECRET = "KYTJxAdhHkJ1ccx7uy9HIgKn";
-    private static final String CALLBACK_URI = "http://localhost:8080/ComicsZoneTracker/resources/templates/unauthorized/unauthorized.jsf";
+    private static final String CALLBACK_URI = "http://localhost:8080/ComicsZoneTracker/resources/templates/unauthorized/redirect_page.jsf";
     private static final Iterable<String> SCOPE = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile;https://www.googleapis.com/auth/userinfo.email".split(";"));
     private static final String USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
@@ -33,11 +39,22 @@ public class SocialNetwork {
     private  GoogleAuthorizationCodeFlow flow;
 
     private String userUrl;
+    private String authCode;
     
     public String getUserUrl()
     {
         return userUrl;
     }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+    
+    
     
     @PostConstruct
     public void buildUserUrl()
@@ -47,4 +64,30 @@ public class SocialNetwork {
         final GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
         userUrl= url.setRedirectUri(CALLBACK_URI).build();
     }
+    public String fetchPersonalInfo() throws IOException{
+
+//            logger.info(authCode);
+//            logger.info(flow.newTokenRequest(authCode).setRedirectUri(CALLBACK_URI));
+//            flow.
+		final GoogleTokenResponse response = flow.newTokenRequest(authCode).setRedirectUri(CALLBACK_URI).execute();
+//		logger.info("response.getAccessToken()"+response.getAccessToken());
+                final Credential credential = flow.createAndStoreCredential(response, null);
+//                setCredential(credential);
+//                credential
+		final HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(credential);
+		// Make an authenticated request
+		final GenericUrl url = new GenericUrl(USER_INFO_URL);
+//                logger.info("genericUrl="+url.);
+		final HttpRequest request = requestFactory.buildGetRequest(url);
+//                HttpRequset requset=new HttpRequest();
+//                logger.info(request.getContent().getEncoding());
+                request.setUrl(new GenericUrl(USER_INFO_URL+"?access_token0="+credential.getAccessToken()));
+//                logger.info("request.getUrl().build()="+request.getUrl().build());
+		request.getHeaders().setContentType("application/json");
+//                logger.info("parameters="+request.getContent().getEncoding());
+		final String jsonIdentity = request.execute().parseAsString();
+//                                logger.info("!!!"+credential.getAccessToken()+"___AAAAAAA_____"+credential.getRefreshToken());
+		return jsonIdentity;
+
+	}
 }
