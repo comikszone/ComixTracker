c4438964d389aef3cc6220904cd568e15270e57f
AShevtsov <shevcov1994@mail.ru>
2015-01-18 14:50:00 +0300

    Fix last commit;

--- ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/userbeans/UserProfileManagedBean.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/userbeans/UserProfileManagedBean.java
@@ -1,43 +1,99 @@
 package com.comicszone.managedbeans.userbeans;
 
 import com.comicszone.dao.userdao.UserDataFacade;
+import com.comicszone.entitynetbeans.Users;
+import java.io.ByteArrayInputStream;
 import java.security.Principal;
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.DefaultUploadedFile;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @SessionScoped
-public class UserManagedBean {
+public class UserProfileManagedBean {
 
+    private UploadedFile newAvatar;
+    Users user;
     @EJB
     UserDataFacade userDAO;
 
-    public String getNickname() {
-        return userDAO.getNickname();
+    public UploadedFile getNewAvatar() {
+        return newAvatar;
     }
 
-    public byte[] getAvatar() {
-        return userDAO.getAvatar();
+    public void setNewAvatar(UploadedFile newAvatar) {
+        this.newAvatar = newAvatar;
+    }
+
+    public String getNickname() {
+        return user.getNickname();
+    }
+
+    public void setNickname(String nickname) {
+        user.setNickname(nickname);
     }
 
     public int getSex() {
-        return userDAO.getSex();
+        return user.getSex();
     }
 
-    public Date getBirthday() {
-        return userDAO.getBirthday();
+    public void setSex(int sex) {
+        user.setSex(sex);
+    }
+
+    public String getBirthday() {
+        if (user.getBirthday() == null) return "";
+        DateFormat df = new SimpleDateFormat("ddMMyyyy");
+        System.out.println(df.format(user.getBirthday()));
+        return df.format(user.getBirthday());
+    }
+
+    public void setBirthday(String birthday) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            user.setBirthday(df.parse(birthday));
+        } catch (ParseException ex) {
+            Logger.getLogger(UserProfileManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public String getEmail() {
-        return userDAO.getEmail();
+        return user.getEmail();
     }
-    
-    public void setCurrentUser(){
+
+    public void setEmail(String email) {
+        user.setEmail(email);
+    }
+
+    public StreamedContent getAvatar() {
+        if (user.getAvatar() == null) {
+            return new DefaultStreamedContent(new ByteArrayInputStream(new byte[0]));
+        }
+        return new DefaultStreamedContent(new ByteArrayInputStream(user.getAvatar()));
+    }
+
+    public void saveChanges() {
+        if (newAvatar != null){
+            user.setAvatar(newAvatar.getContents());
+        }
+        userDAO.edit(user);
+    }
+
+    @PostConstruct
+    public void setCurrentUser() {
         Principal prin = FacesContext.getCurrentInstance()
                 .getExternalContext().getUserPrincipal();
-        userDAO.findUserByNickname(prin.getName());
+        user = userDAO.getUserWithNickname(prin.getName());
     }
 }
