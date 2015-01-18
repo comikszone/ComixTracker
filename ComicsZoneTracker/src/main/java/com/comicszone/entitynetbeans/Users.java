05f27d0c9c8b43f454b1dae62c91542a1a0b4e64
AShevtsov <shevcov1994@mail.ru>
2015-01-18 19:19:12 +0300

    Added method registration(Users user, String password); Removed annotation "@NotNull" for field "email" in class "Users";

--- ComicsZoneTracker/src/main/java/com/comicszone/entitynetbeans/Users.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/entitynetbeans/Users.java
@@ -77,7 +77,6 @@ public class Users implements Serializable {
     private Date birthday;
     // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Недопустимый адрес электронной почты")//if the field contains email address consider using this annotation to enforce field validation
     @Basic(optional = false)
-    @NotNull
     @Size(min = 1, max = 2147483647)
     @Column(name = "email")
     private String email;
