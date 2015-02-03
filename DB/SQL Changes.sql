6a69fde008e885d194f5e1a6b4fcbefa86deab99
Phoenix1092 <phoenix1092@mail.ru>
2015-02-03 16:33:36 +0400

    Added PKs for rating tables

--- DB/SQL Changes.sql
+++ DB/SQL Changes.sql
@@ -52,14 +52,17 @@ ALTER TABLE messages ADD COLUMN msg_time timestamp;
 
 ALTER TABLE comments ADD COLUMN comment_time timestamp;
 
-CREATE TABLE ucrating (user_id Integer NOT NULL, comics_id Integer NOT NULL, rating Real NOT NULL);
+CREATE TABLE ucrating  (user_id Integer NOT NULL, comics_id Integer NOT NULL, rating Real NOT NULL);
 ALTER TABLE  ucrating  ADD CONSTRAINT  c_rated_by  FOREIGN KEY ( user_id ) REFERENCES  users  ( user_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE  ucrating  ADD CONSTRAINT  c_rated  FOREIGN KEY ( comics_id ) REFERENCES  comics  ( comics_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE  ucrating  ADD CONSTRAINT  Key20 PRIMARY KEY (comics_id,user_id);
 
-CREATE TABLE uvrating (user_id Integer NOT NULL, volume_id Integer NOT NULL, rating Real NOT NULL);
+CREATE TABLE uvrating  (user_id Integer NOT NULL, volume_id Integer NOT NULL, rating Real NOT NULL);
 ALTER TABLE  uvrating  ADD CONSTRAINT  v_rated_by  FOREIGN KEY ( user_id ) REFERENCES  users  ( user_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE  uvrating  ADD CONSTRAINT  v_rated  FOREIGN KEY ( volume_id ) REFERENCES  volume  ( volume_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE  uvrating  ADD CONSTRAINT  Key21 PRIMARY KEY (volume_id,user_id);
 
-CREATE TABLE uirating (user_id Integer NOT NULL, issue_id Integer NOT NULL, rating Real NOT NULL);
+CREATE TABLE uirating  (user_id Integer NOT NULL, issue_id Integer NOT NULL, rating Real NOT NULL);
 ALTER TABLE  uirating  ADD CONSTRAINT  i_rated_by  FOREIGN KEY ( user_id ) REFERENCES  users  ( user_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE  uirating  ADD CONSTRAINT  i_rated  FOREIGN KEY ( issue_id ) REFERENCES  issue  ( issue_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE  uvrating  ADD CONSTRAINT  Key22 PRIMARY KEY (issue_id,user_id);
