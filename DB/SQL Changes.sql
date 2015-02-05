1ceb0b75ee9b25374060a8e04e26345cff95a8c5
Phoenix1092 <phoenix1092@mail.ru>
2015-02-05 05:04:48 +0400

    Added source fields
    Added source field for character, issue, volume and comics tables.

--- DB/SQL Changes.sql
+++ DB/SQL Changes.sql
@@ -66,3 +66,8 @@ CREATE TABLE uirating  (user_id Integer NOT NULL, issue_id Integer NOT NULL, rat
 ALTER TABLE  uirating  ADD CONSTRAINT  i_rated_by  FOREIGN KEY ( user_id ) REFERENCES  users  ( user_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE  uirating  ADD CONSTRAINT  i_rated  FOREIGN KEY ( issue_id ) REFERENCES  issue  ( issue_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE  uirating  ADD CONSTRAINT  Key22 PRIMARY KEY (issue_id,user_id);
+
+ALTER TABLE character ADD source Varchar;
+ALTER TABLE issue ADD source Varchar;
+ALTER TABLE volume ADD source Varchar;
+ALTER TABLE comics ADD source Varchar;
