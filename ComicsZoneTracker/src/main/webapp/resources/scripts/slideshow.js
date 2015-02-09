abd15d46a90e633e32004d711b89c3031c89c2e4
ajpyatakov <ajpyatakov@gmail.com>
2015-02-09 11:44:07 +0300

    Still playing with slick.

--- ComicsZoneTracker/src/main/webapp/resources/scripts/slideshow.js
+++ ComicsZoneTracker/src/main/webapp/resources/scripts/slideshow.js
@@ -1,38 +1,23 @@
 $(document).ready(function(){
       $('.slideshow').slick({
         dots: true,
-        infinite: true,
-        speed: 600,
         arrows: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
         adaptiveHeight: true,
         autoplay: true,
         autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: '0px',
         responsive: [
         {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
+            breakpoint: 960,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '20px'
             }
-        }   
+        } 
         ]
     });
 });
