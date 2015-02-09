f8df36fee79b44adffb23accf66539227cb776ca
ajpyatakov <ajpyatakov@gmail.com>
2015-02-09 14:55:03 +0300

    Changed progress architecture and added REST service.

--- ComicsZoneTracker/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ ComicsZoneTracker/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -36,6 +36,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.comicszone.dao.ProgressFacade.class);
         resources.add(com.comicszone.dao.commentsdao.CommentsFacade.class);
         resources.add(com.comicszone.webservice.JsonProducer.class);
     }
