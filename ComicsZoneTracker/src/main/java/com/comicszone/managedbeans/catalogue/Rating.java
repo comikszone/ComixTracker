5db8472dbe2216af3cbe3d44d02be04ea5e83299
Dmitry Eschenko <yeschenko_dmitry@mail.ru>
2015-02-19 19:54:55 +0300

    Filtering works just ones.

--- ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/catalogue/Rating.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/catalogue/Rating.java
@@ -34,5 +34,35 @@ public class Rating {
     public void setImage(String image) {
         this.image = image;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + (this.value != null ? this.value.hashCode() : 0);
+        hash = 71 * hash + (this.image != null ? this.image.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Rating other = (Rating) obj;
+        if (this.value != other.value && (this.value == null || !this.value.equals(other.value))) {
+            return false;
+        }
+        if ((this.image == null) ? (other.image != null) : !this.image.equals(other.image)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" + "value=" + value + ", image=" + image + '}';
+    }
 }
