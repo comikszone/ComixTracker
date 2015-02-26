1073ef48e2bad55e669f5af237a81c6cef96004c
Eschenko Dmitry <freeman.1994@mail.ru>
2015-02-26 18:25:58 +0400

    minor changes

--- ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/catalogue/ComicsFinderManagedBean.java
+++ ComicsZoneTracker/src/main/java/com/comicszone/managedbeans/catalogue/ComicsFinderManagedBean.java
@@ -25,11 +25,11 @@ public class ComicsFinderManagedBean implements Serializable {
     @PostConstruct
     public void init() {
         ratings = new ArrayList();
-        ratings.add(new Rating(0,"/resources/images/ratings/1.jpg"));
-        ratings.add(new Rating(1,"/resources/images/ratings/2.jpg"));
-        ratings.add(new Rating(2,"/resources/images/ratings/3.jpg"));
-        ratings.add(new Rating(3,"/resources/images/ratings/4.jpg"));
-        ratings.add(new Rating(4,"/resources/images/ratings/5.jpg"));
+        ratings.add(new Rating(0,"/resources/images/ratings/AllRatings.jpg"));
+        ratings.add(new Rating(1,"/resources/images/ratings/1.jpg"));
+        ratings.add(new Rating(2,"/resources/images/ratings/2.jpg"));
+        ratings.add(new Rating(3,"/resources/images/ratings/3.jpg"));
+        ratings.add(new Rating(4,"/resources/images/ratings/4.jpg"));
     }
     
     public Rating getRating(Integer value) {
